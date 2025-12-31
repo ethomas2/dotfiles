c03fa226106b4c512f775e9bf6accfa84f8439cc
Evan Thomas <evanrthom@gmail.com>
2025-12-30 22:16:59 -0600

    Add mgrep-watch.d

---
+++ scripts/mgrep-watch.d
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+trap 'exit' INT # necessary bc otherwise just kills the running mgrep watch. Not the whole bash
+cd /Users/evanthomas/notes/Main
+while true; do
+  timeout --foreground 3600 mgrep watch
+done
+
+
+# while true; do
+#   mgrep watch &
+#   pid=$!
+
+#   ( sleep 3600; kill "$pid" ) &
+#   killer=$!
+
+#   wait "$pid"       # wait for mgrep to exit (naturally or via kill)
+#   kill "$killer" 2>/dev/null || true  # cleanup in case mgrep exited early
+# done
